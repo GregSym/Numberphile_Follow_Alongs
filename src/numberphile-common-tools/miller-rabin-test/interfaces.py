9d547615a9721be557d64605c383481b7c8d2286
GregSym <gtsrex@gmail.com>
2021-12-01 23:13:33 +0200

    style update to interfaces

--- src/numberphile-common-tools/miller-rabin-test/interfaces.py
+++ src/numberphile-common-tools/miller-rabin-test/interfaces.py
@@ -6,18 +6,30 @@ from typing import Protocol
 
 
 class StarWitnessesInterface(Protocol):
-  """ An interface for interacting with defined 
-      test cases for the Miller Rabin Test 
-  """
-  @property
-  def star_witnesses(self) -> tuple[int, ...]:...
-  @property
-  def upper_bound(self) -> int:...
+    """An interface for interacting with defined
+    test cases for the Miller Rabin Test
+    """
+
+    @property
+    def star_witnesses(self) -> tuple[int, ...]:
+        ...
+
+    @property
+    def upper_bound(self) -> int:
+        ...
 
 
 class _PrimeDetectorInterface(Protocol):
-    """ the methods to expect on a prime calculator from this package """
-    def prime_determination(self, 
-                            n: int, 
-                            witness_group: StarWitnessesInterface# = SmallestPossible()
-                            ) -> bool:... 
+    """the methods to expect on a prime calculator from this package"""
+
+    def prime_determination(
+        self, n: int, witness_group: StarWitnessesInterface  # = SmallestPossible()
+    ) -> bool:
+        ...
+
+
+class _VisualisationInterface(Protocol):
+    def __call__(map_of_liars_to_count: dict[int, int], verbose: bool = False) -> None:
+        ...
+
+    """ defines the function that takes the sorted dict object and visualises the data """
