5a8914d797d67720b862ba7240c528b3cec4bcdc
GregSym <gtsrex@gmail.com>
2021-12-01 23:18:39 +0200

    added tox, started pkging common tools

---
+++ src/numberphile-common-tools/miller-rabin-test/__init__.py
@@ -0,0 +1,6 @@
+from interfaces import StarWitnessesInterface
+from interfaces import _PrimeDetectorInterface
+from interfaces import _VisualisationInterface
+
+
+__all__ = ["StarWitnessInterface", "_PrimeDetectorInterface", "_VisualisationInterface"]
