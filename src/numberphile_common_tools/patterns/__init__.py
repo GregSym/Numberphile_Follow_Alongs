b1dd842c51d844b79bda4187be62210145409b6b
GregSym <gtsrex@gmail.com>
2022-01-08 22:44:16 +0200

    updated patterns init

--- src/numberphile_common_tools/patterns/__init__.py
+++ src/numberphile_common_tools/patterns/__init__.py
@@ -1,9 +1,13 @@
 from .hitomezashi_stitch_pattern import HitomezashiRepr
 from .hitomezashi_stitch_pattern import _VisualiserInterfaceHitomezashi
 from .hitomezashi_stitch_pattern import visualise_hitomezashi_pattern
+from .comparisons import _StrComparablePattern
+from .generic_maze import Maze
 
 __all__ = [
     "HitomezashiRepr",
     "visualise_hitomezashi_pattern",
     "_VisualiserInterfaceHitomezashi",
+    "_StrComparablePattern",
+    "Maze",
 ]
---
+++ src/numberphile_common_tools/patterns/comparisons.py
@@ -0,0 +1,17 @@
+import sys
+from typing import Any
+
+if float(f"{sys.version_info[0]}.{sys.version_info[1]}") >= 3.8:
+    from typing import Protocol
+else:
+    print("python version lower than 3.9 detected")
+    from typing_extensions import (  # type: ignore
+        Protocol,
+    )  # < 3.8 Protocol lives in a different package
+
+
+class _StrComparablePattern(Protocol):
+    def __call__(self, *args: Any, **kwds: Any) -> str:
+        """
+        returns a kind of str repr of the pattern for comparison to other datastructures of the same pattern
+        """
